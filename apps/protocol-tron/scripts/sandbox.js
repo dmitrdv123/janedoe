ab2f94cd2cb6fc30229a332a08e363895350c484
dmitrdv <dmitrdv@gmail.com>
2024-04-29 17:54:03 +0500

    deploying wrapped native to tron

--- apps/protocol-tron/scripts/sandbox.js
+++ apps/protocol-tron/scripts/sandbox.js
@@ -1,13 +1,15 @@
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}`.trim() })
+
 const { TronWeb } = require('tronweb')
 const WrappedNative = require('../build/contracts/WrappedNative.json')
 
 const tronWeb = new TronWeb({
-  fullHost: 'http://127.0.0.1:9090',
-  privateKey: 'a0dbe220a910eaa05a5e038058515ddf977ad3496e82ebd49dc702e305439d8f'
-});
+  fullHost: process.env.RPC,
+  privateKey: process.env.SIGNER
+})
 
 async function sendMessage() {
-  const contractAddress = '4137b3ec9db66550a884008d8268ae1ef9de79e071'
+  const contractAddress = '418d9c8c9678751004eef8c16c96f77f2f6806cd69'
   const contract = await tronWeb.contract(WrappedNative.abi, contractAddress)
   const symbol = await contract.symbol().call()
   console.log(`WrappedNative symbol: ${symbol}`)
